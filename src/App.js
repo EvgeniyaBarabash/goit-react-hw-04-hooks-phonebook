c249f238a4706744bb7fd38788db245ebc7a96b6
EvgeniyaBarabash <barabash1989@gmail.com>
2021-11-12 22:20:27 +0200

    localStorage

--- src/App.js
+++ src/App.js
@@ -10,6 +10,18 @@ class App extends Component {
     contacts: initialContacts,
     filter: '',
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   addContact = (name, number) => {
     const contact = {
       id: uuidv4(),
